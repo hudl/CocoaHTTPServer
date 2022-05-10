1e19e354dee69a0614ab1446c99601db271d9866
pluddy <patrick.luddy@hudl.com>
2022-05-10 11:06:31 -0500

    Swift Package Manager Support

---
+++ Tests/CocoaHTTPServerTests/CocoaHTTPServerTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CocoaHTTPServer
+
+final class CocoaHTTPServerTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+//        XCTAssertEqual(CocoaHTTPServer().text, "Hello, World!")
+    }
+}

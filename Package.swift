64b93db514bb9dd7b2afc7f232761dc481f6f03e
Patrick Luddy <patrick.luddy@hudl.com>
2025-07-02 14:31:07 +0100

    include

--- Package.swift
+++ Package.swift
@@ -22,7 +22,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CocoaHTTPServer",
-            dependencies: ["CocoaLumberjack", "CocoaAsyncSocket"]),
+            dependencies: ["CocoaLumberjack", "CocoaAsyncSocket"],
+            publicHeadersPath: "include"),
         .testTarget(
             name: "CocoaHTTPServerTests",
             dependencies: ["CocoaHTTPServer"]),
